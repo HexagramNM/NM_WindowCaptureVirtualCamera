13e1b705f42e782ac27f04bcd9c3d51755a5b3f5
HexagramNM <hexagram_n.m@jcom.home.ne.jp>
2024-12-08 15:44:47 +0900

    Create setupRepository.bat

---
+++ setupRepository.bat
@@ -0,0 +1,11 @@
+@echo off
+
+cd External\windows-classic-samples
+
+git config core.sparsecheckout true
+
+cd ..\..
+
+echo Samples/Win7Samples > .git\modules\External\windows-classic-samples\info\sparse-checkout
+
+git -C External/windows-classic-samples read-tree -mu HEAD
