f88cb4469948cbad66878f81ef0ea93d8756e47c
HexagramNM <hexagram_n.m@jcom.home.ne.jp>
2024-12-08 15:49:07 +0900

    Update setupRepository.bat

--- setupRepository.bat
+++ setupRepository.bat
@@ -6,6 +6,6 @@ git config core.sparsecheckout true
 
 cd ..\..
 
-echo Samples/Win7Samples > .git\modules\External\windows-classic-samples\info\sparse-checkout
+echo Samples/Win7Samples/multimedia/directshow/baseclasses > .git\modules\External\windows-classic-samples\info\sparse-checkout
 
 git -C External/windows-classic-samples read-tree -mu HEAD
